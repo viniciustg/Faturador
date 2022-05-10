8d893f36aeaeed59a70013e14939b439f940bc63
ViniciusTG <User@DESKTOP-27FEHD8>
2022-05-10 20:22:46 -0300

    Adicionar arquivos de projeto.

---
+++ Faturador/Domain/FiltroFaturamento.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Faturador.Domain
+{
+    public class FiltroFaturamento
+    {
+        public int Id { get; set; }
+        public int CodigoCliente { get; set; }
+        public DateTime DataInicioApuracao { get; set; }
+        public DateTime DataFimApuracao { get; set; }
+        public int TipoApuracao { get; set; }
+        public int GerenciadoraId { get; set; }
+    }
+}
---
+++ Faturador/Services/FaturadorService.cs
@@ -0,0 +1,17 @@
+﻿using Faturador.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Faturador.Services
+{
+    public class FaturadorService
+    {
+        public void Faturar(FiltroFaturamento filtro)
+        {
+            Console.WriteLine($"Fatura: {filtro.Id} | Cliente: {filtro.CodigoCliente} | GERADA COM SUCESSO | Data Inicio Apuração: {filtro.DataInicioApuracao} - Data Fim Apuração: {filtro.DataFimApuracao}");
+        }        
+    }
+}
